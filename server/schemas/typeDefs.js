56b0542135c541d002e55c8e7b6a08b529eb0282
Dave Quinn <dquinn8@cogeco.ca>
2022-01-11 14:49:04 -0500

    added reactions to appolo thought schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,15 @@ type Thought {
     createdAt: String
     username: String
     reactionCount: Int
-  }
+    reactions: [Reaction]
+}
+
+type Reaction {
+  _id: ID
+  reactionBody: String
+  createdAt: String
+  username: String
+}
 
   type Query {
     thoughts(username: String): [Thought]
