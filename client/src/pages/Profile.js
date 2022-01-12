fce53479b615495016e69dc3f45d162703c37193
Dave Quinn <dquinn8@cogeco.ca>
2022-01-12 13:37:07 -0500

    Profile page can now show a logged in users info or a requested users info

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
@@ -9,11 +9,11 @@ import FriendList from '../components/FriendList';
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
 
   if (loading) {
     return <div>Loading...</div>;
