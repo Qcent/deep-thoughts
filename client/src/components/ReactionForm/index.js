46181c449d0c1f2cf2c967397115060699853aea
Dave Quinn <dquinn8@cogeco.ca>
2022-01-14 00:05:51 -0500

    created ADD_REACTION mutation and implemented in ReactionForm with error handling

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_REACTION } from '../../utils/mutations'
 
 const ReactionForm = ({ thoughtId }) => {
 
     const [reactionBody, setBody] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+    const [addReaction, { error }] = useMutation(ADD_REACTION);
 
     const handleChange = event => {
     if (event.target.value.length <= 280) {
@@ -14,14 +17,25 @@ const ReactionForm = ({ thoughtId }) => {
 
     const handleFormSubmit = async event => {
     event.preventDefault();
-    setBody('');
-    setCharacterCount(0);
+    try {
+        // add thought to database
+        await addReaction({
+          variables: { reactionBody, thoughtId }
+        });
+    
+        // clear form value
+        setBody('');
+        setCharacterCount(0);
+      } catch (e) {
+        console.error(e);
+      }
     };
 
   return (
     <div>
-      <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
         Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
       <form className="flex-row justify-center justify-space-between-md align-stretch"
       onSubmit={handleFormSubmit}>
