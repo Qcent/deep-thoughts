e3e46f40cfdd48babfd0a9957fc527a4fbb09eb6
Dave Quinn <dquinn8@cogeco.ca>
2022-01-13 23:19:27 -0500

    add friend button now conditionally renders on profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -51,9 +51,12 @@ const Profile = () => {
       <h2 className="bg-dark text-secondary p-3 display-inline-block">
         Viewing {userParam ? `${user.username}'s` : 'your'} profile.
       </h2>
-      <button className="btn ml-auto" onClick={handleClick}>
-        Add Friend
-      </button>
+      {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
+      )}
+
       </div>
 
       <div className="flex-row justify-space-between mb-3">
